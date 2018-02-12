45697d2936619628525bdb52a9926d5cd2faa61a
Laura Jaime Villamayor <laurajaime@MacBook-Pro-de-Laura.local>
2018-02-12 19:04:52 +0100

    Search in web

--- Nokogiri-demo.rb
+++ Nokogiri-demo.rb
@@ -4,5 +4,8 @@ require 'open-uri'
 
 page = Nokogiri::HTML(open("https://www.ruby-lang.org/es/"))
 links = page.css("a")
+title = page.css("h3")
 puts links.length
 puts links[0].text
+puts title.length
+puts title[0].text
